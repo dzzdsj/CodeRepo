f346afac1fd2a1a8b436ed37a736595933a6e3d9
cp <chenping@syswin.com>
2023-04-12 13:23:29 +0800

    更新描述

--- java/design-pattern/design-pattern-demo/src/main/java/com/dzzdsj/design/pattern/demo/creational/AbstractFactoryPattern/Client/AbstractFactoryPatternDemo.java
+++ java/design-pattern/design-pattern-demo/src/main/java/com/dzzdsj/design/pattern/demo/creational/AbstractFactoryPattern/Client/AbstractFactoryPatternDemo.java
@@ -6,7 +6,16 @@ import com.dzzdsj.design.pattern.demo.creational.AbstractFactoryPattern.Abstract
 import com.dzzdsj.design.pattern.demo.creational.AbstractFactoryPattern.AbstractFactory.FactoryProducer;
 
 /**
- * 抽象工厂模式,
+ * 抽象工厂模式
+ * 意图：
+ * 提供一个创建一系列相关或相互依赖的对象的接口，而无需指定具体的类
+ * client类只操作抽象类（工厂、产品）的接口
+ *
+ * 适用性：
+ * 当系统要独立它的产品的创建、组合和表示时
+ * 当系统要由多个产品系列中的一个来配置时
+ * 当要强调系列产品的相关性，以便联合使用时
+ * 当提供的产品只想暴露接口，而不是实现时
  */
 public class AbstractFactoryPatternDemo {
     public static void main(String[] args) {
